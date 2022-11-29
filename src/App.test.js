876754e69fff880e43d82b2e1df4a572c8b5739f
hunterphillips <hkphillips42@gmail.com>
2022-11-29 11:57:52 -0600

    homepage and manifest update

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import { render, screen } from '@testing-library/react';
+import App from './App';
 
-test("renders HN Search header", () => {
-	render(<App />);
-	const headerText = screen.getByText(/HN Search/i);
-	expect(headerText).toBeInTheDocument();
+test('renders Hacker News Search header', () => {
+  render(<App />);
+  const headerText = screen.getByText(/Hacker News Search/i);
+  expect(headerText).toBeInTheDocument();
 });
