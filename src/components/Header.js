8b026a054f440223484a9063a8fa2e2823394c4b
hunterphillips <hkphillips42@gmail.com>
2022-11-29 12:55:01 -0600

    style updates

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,11 @@ import lightOffImg from '../assets/lightbulb_off.png';
 export const Header = ({ lightOn, setLightOn, onClick }) => {
   return (
     <div style={{ position: 'relative' }}>
-      <h1 className="AppHeader" onClick={onClick}>
+      <h1 className="App-header" onClick={onClick}>
         Hacker News Search
       </h1>
       <img
-        className="lightBulb"
+        className="lightbulb"
         onClick={() => setLightOn(!lightOn)}
         src={lightOn ? lightOnImg : lightOffImg}
         alt="lightbulb"
