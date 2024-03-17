31df7d09d2f0b69fc35025eb48079b893e3e2910
hunterphillips <hkphillips42@gmail.com>
2024-03-17 09:29:56 -0500

    typescript

---
+++ src/interfaces.ts
@@ -0,0 +1,9 @@
+export interface NewsArticle {
+  objectID: string;
+  url: string;
+  title: string;
+  story_title?: string;
+  author: string;
+  points: number;
+  created_at: string;
+}
---
+++ src/types.ts
@@ -0,0 +1,1 @@
+export type NewsArticleProperty = 'title' | 'author' | 'points' | 'created_at';
